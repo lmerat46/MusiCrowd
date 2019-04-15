93b9149fe99fdda19818b697760ab0da5b403754
merat.laure@hotmail.fr <Mathildemerat1999>
2019-04-15 15:22:17 +0200

    rectification des create table

--- create_All.sql
+++ create_All.sql
@@ -1,15 +1,16 @@
-DROP TABLE IF EXISTS MusiCrowd.Participant;
-DROP TABLE IF EXISTS MusiCrowd.Beneficiaire;
-DROP TABLE IF EXISTS MusiCrowd.Projet;
-DROP TABLE IF EXISTS MusiCrowd.Reward;
-DROP TABLE IF EXISTS MusiCrowd.ProjetTaxe;
 DROP TABLE IF EXISTS MusiCrowd.Participation;
+DROP TABLE IF EXISTS MusiCrowd.Reward;
+DROP TABLE IF EXISTS MusiCrowd.Projet;
+DROP TABLE IF EXISTS MusiCrowd.Utilisateur;
+
+
+
 
 DROP SCHEMA IF EXISTS MusiCrowd;
 
 CREATE SCHEMA MusiCrowd;
 
-CREATE TABLE MusiCrowd.Participant (
+CREATE TABLE MusiCrowd.Utilisateur (
 	user_id SERIAL,
 	mdp varchar(255) NOT NULL,
 	nom varchar(255) NOT NULL,
@@ -19,22 +20,9 @@ CREATE TABLE MusiCrowd.Participant (
 	mail varchar(255) NOT NULL,
 	rib varchar(255) UNIQUE,
 	nb_projet_supportes INT,
-	PRIMARY KEY(user_id)
-);
-
-CREATE TABLE MusiCrowd.Beneficiaire (
-	user_id SERIAL,
-	mdp varchar(255) NOT NULL,
-	nom varchar(255) NOT NULL,
-	prenom varchar(255) NOT NULL,
-	adresse varchar(255) NOT NULL,
-	telephone varchar(255) NOT NULL,
-	mail varchar(255) NOT NULL,
-	rib varchar(255) UNIQUE,
 	nb_projet_crees INT,
 	PRIMARY KEY(user_id)
 );
-
 
 CREATE TABLE MusiCrowd.Projet (
 	projet_id SERIAL,
@@ -45,8 +33,10 @@ CREATE TABLE MusiCrowd.Projet (
 	date_fin date NOT NULL,
 	objectif INT,
 	somme_recoltee INT,
+	taxe_perc INT NOT NULL,
+	somme_percue INT,
 	PRIMARY KEY(projet_id),
-	FOREIGN KEY(user_id) REFERENCES Beneficiaire(user_id),
+	FOREIGN KEY(user_id) REFERENCES MusiCrowd.Utilisateur(user_id),
 	CHECK (date_deb >= date(NOW())),
 	CHECK (date_deb < date_fin),
 	CHECK (date_fin > date_deb),
@@ -62,18 +52,10 @@ CREATE TABLE MusiCrowd.Reward (
 	somme_min INT,
 	somme_max INT,
 	PRIMARY KEY(reward_id),
-	FOREIGN KEY(projet_id) REFERENCES Projet(projet_id),
+	FOREIGN KEY(projet_id) REFERENCES MusiCrowd.Projet(projet_id),
 	CHECK (somme_min > 0),
 	CHECK (somme_min < somme_max),
 	CHECK (somme_max > somme_min)
-);
-
-CREATE TABLE MusiCrowd.ProjetTaxe (
-	projet_id SERIAL,
-	taxe_perc INT NOT NULL,
-	somme_percue INT,
-	PRIMARY KEY(projet_id),
-	FOREIGN KEY(projet_id) REFERENCES projet(projet_id)
 );
 
 CREATE TABLE MusiCrowd.Participation (
@@ -83,9 +65,9 @@ CREATE TABLE MusiCrowd.Participation (
 	montant INT,
 	date_p date NOT NULL,
 	PRIMARY KEY(user_id, projet_id, reward_id),
-	FOREIGN KEY(user_id) REFERENCES Participant(user_id),
-	FOREIGN KEY(projet_id) REFERENCES Projet(projet_id),
-	FOREIGN KEY(reward_id) REFERENCES Reward(reward_id),
+	FOREIGN KEY(user_id) REFERENCES MusiCrowd.Utilisateur(user_id),
+	FOREIGN KEY(projet_id) REFERENCES MusiCrowd.Projet(projet_id),
+	FOREIGN KEY(reward_id) REFERENCES MusiCrowd.Reward(reward_id),
 	CHECK (montant > 0)
 );
 
