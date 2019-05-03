e46f7c7703707a7ce9f7e302e352d01d91b3e27a
merat.laure@hotmail.fr <Mathildemerat1999>
2019-05-03 17:04:28 +0200

    ajout des transactions dans fichier test001

--- test_ajout_archivage/test_001.sql
+++ test_ajout_archivage/test_001.sql
@@ -1,1 +1,3 @@
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 INSERT INTO MusiCrowd.Participation (user_id, projet_id, montant, date_p) VALUES (6,2, 2000, '2019-03-26');
+COMMIT;
--- test_ajout_reward/test_001.sql
+++ test_ajout_reward/test_001.sql
@@ -1,1 +1,3 @@
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 INSERT INTO MusiCrowd.Reward (projet_id, nom_cadeau, detail_cadeau, somme_min, somme_max) VALUES (1, 'Reconnaissance', 'reconnaissance éternelle du dieu de la guitare: Splash', 1, 200000);
+COMMIT;
